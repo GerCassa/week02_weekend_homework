2c3fa7c1532b5f3b74cfa8a25c27447a66794bf7
GerCassa <gercassa@gmail.com>
2018-08-10 10:58:10 +0100

    added test_guest_has_name

---
+++ specs/guest_spec.rb
@@ -0,0 +1,15 @@
+require ("minitest/autorun")
+require ("minitest/rg")
+require_relative ("../guest")
+
+class GuestTest < MiniTest::Test
+
+  def setup
+    @guest = Guest.new("Ozzy", 50, "Planet Caravan")
+  end
+
+  def test_guest_has_name
+    assert_equal("Ozzy", @guest.name)
+  end
+
+end
